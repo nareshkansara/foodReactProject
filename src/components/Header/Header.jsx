25e1821285a0b469a8f16a1955a0567038c018ee
Naresh Kansara <nareshkansara06@gmail.com>
2024-06-11 20:40:00 +0530

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your food here</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat
+          molestiae magnam
+        </p>
+        <button>View More</button>
+      </div>
+    
+    </div>
+  );
+};
+
+export default Header;
